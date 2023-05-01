1168b9014374a5b52a6353f342efaf17e2d8f6e4
charlesanderson25 <charles_spfc@hotmail.com>
2023-05-01 10:35:34 -0300

    Limpeza do dos arquivos e reset css

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,13 @@
+import "./banner.css";
+
 function Banner() {
   return (
-    <header>
-      <img src="../public/pictures/banner.png" alt="Banner Organo" />
+    <header className="banner">
+      <img
+        id="logo-organo"
+        src="../public/pictures/banner.png"
+        alt="Banner Organo"
+      />
     </header>
   );
 }
