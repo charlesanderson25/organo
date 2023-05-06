8de53b920b9f7efbc13dc43de0cc000a2b890f07
charlesanderson25 <charles_spfc@hotmail.com>
2023-05-06 11:01:45 -0300

    Install tailwind

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -2,16 +2,16 @@ import InputField from "../input/Input";
 import "./form.css";
 
 const FormOrgano = () => {
-    return (
-        <div className="form-container">
-            <form id="form">
-                <h2>Preencha os dados para criar o card do colaborador</h2>
-                <InputField label="Nome" placeholder="Digite Seu Nome" />
-                <InputField label="Cargo" placeholder="Informe Seu Cargo"/>
-                <InputField label="Imagem" placeholder="Informe o Endereço da Imagem" />
-            </form>
-        </div>
-    )
-}
+  return (
+    <div className="form-container flex-row my-20">
+      <form id="form">
+        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <InputField label="Nome" placeholder="Digite Seu Nome" />
+        <InputField label="Cargo" placeholder="Informe Seu Cargo" />
+        <InputField label="Imagem" placeholder="Informe o Endereço da Imagem" />
+      </form>
+    </div>
+  );
+};
 
 export default FormOrgano;
