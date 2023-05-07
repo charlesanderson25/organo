e6f1dc16e75605d564705a574007fc575a0426e1
charlesanderson25 <charles_spfc@hotmail.com>
2023-05-07 12:03:30 -0300

    Add valueCaptured

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent } from "react";
 import "./input.css";
 
 interface InputFieldProps {
@@ -7,10 +8,18 @@ interface InputFieldProps {
 }
 
 const InputField = ({ label, placeholder, required }: InputFieldProps) => {
+  let valueCaptured = "";
+
+  const catchValue = (eventoCaptura: ChangeEvent<HTMLInputElement>) => {
+    valueCaptured = eventoCaptura.target.value;
+    console.log(valueCaptured);
+  };
+
   return (
     <div className="input-div">
       <label className="block my-4 text-2xl ">{label}</label>
       <input
+        onChange={catchValue}
         required={required}
         className="bg-white shadow-md w-full py-3 px-1"
         id="input"
