5398b5a7f0dd8b2c723f692923aee83a184081c0
charles <charles_spfc@hotmail.com>
2023-05-07 21:13:17 -0300

    Add selectValue and handleSelect

--- src/components/dropDown/DropDown.tsx
+++ src/components/dropDown/DropDown.tsx
@@ -1,3 +1,5 @@
+import React, {ChangeEvent, useState} from "react";
+
 const equipe = ["ECI", "Gestão de Numerário", "Fiscalização"];
 
 interface DropDownProps {
@@ -6,11 +8,17 @@ interface DropDownProps {
 }
 
 const DropDown = ({ item, label }: DropDownProps) => {
+  const [selectValue, setSelectValue] = useState('');
+
+  const handleSelect = (eventSelect: ChangeEvent<HTMLSelectElement>) => {
+    setSelectValue(eventSelect.target.value);
+  }
+
   return (
     <div className="py-10">
       <label className="text-2xl">
         {label}
-        <select className="block mt-2 text-xl p-1 py-2 shadow-md w-full">
+        <select value={selectValue} onChange={handleSelect} className="block mt-2 text-xl p-1 py-2 shadow-md w-full">
           {equipe.map((equipeItem) => {
             return <option key={equipeItem}>{equipeItem}</option>;
           })}
